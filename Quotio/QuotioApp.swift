d071924b69605454c07c4206b8f081eb3133a044
Trong Nguyen <nguyenphutrong.dev@gmail.com>
2025-12-24 17:08:08 +0700

    Initial Commit

---
+++ Quotio/QuotioApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotioApp.swift
+//  Quotio
+//
+//  Created by Trong Nguyen on 24/12/25.
+//
+
+import SwiftUI
+
+@main
+struct QuotioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
